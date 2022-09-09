dbaf131066d571b32cce592113134f36545dc7ec
Gedion <gedionandme@gmail.com>
2022-09-10 00:00:24 +0300

    Preprocessing data
    - Fixed missing data
    - Performed label encoding
    - Extracted features

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import logging
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 logging.basicConfig(filename='../log/log.log', filemode='a',encoding='utf-8', level=logging.DEBUG)
 
@@ -32,3 +32,7 @@ def fill_mode(df,column):
 def label_encode(df,column):
     df[column] = LabelEncoder().fit_transform(df[column])
     return df[column]
+
+def scale_data(df):
+    df = StandardScaler().fit_transform(df)
+    return df
